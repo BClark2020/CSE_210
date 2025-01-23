780248e1e66f2244a8e13a94ff7efafb712e81e7
BClark21 <brenden.t.clark2020@gmail.com>
2025-01-23 11:05:02 -0700

    Completed task: saved first and last name in string variables and displayed it to the user in the format of James Bond.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+	static void Main(string[] args)
+	{
+		Console.Write("Enter your first name: ");
+		string FirstName = Console.ReadLine();
+		
+	
+		Console.Write("Enter your last name: ");
+		string LastName = Console.ReadLine();
+		
+		Console.WriteLine($"My name is {LastName}, {FirstName} {LastName}.");
+	}   
 }
