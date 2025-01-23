80ad727a475a7ec39021e2a5310ec0f4b2036191
BClark21 <brenden.t.clark2020@gmail.com>
2025-01-23 11:53:19 -0700

    Task completed: The user inputs a grade and the class gradebook decifers it. To examine the second char of the grade I had to convert it back to a string then get the second digit as a char, convert it back to a string and then parse it as an int. If there is any more effecient way to do this I would love to hear suggestions!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,92 @@
 using System;
 
+class Gradebook
+{
+	public string GetGrade()
+	{
+		int grade = 0;
+		bool var = true;
+		do
+		{
+			try
+			{
+				Console.Clear();
+				Console.Write("Enter your grade: ");
+				grade = int.Parse(Console.ReadLine());
+				var = true;
+			}
+			catch (FormatException)
+			{
+				var = false;
+				Console.WriteLine("ERROR: Please enter an integer.");
+				Thread.Sleep(2000);
+			}
+		} while (var != true);
+		
+		string letter_grade = CompareGrade(grade);
+		
+		return letter_grade;	
+	}
+	
+	
+	private string CompareGrade(int grade)
+	{
+		string letter = "F";
+		int second = 0;
+		
+		string sgrade = grade.ToString();
+		try 
+		{
+			char ssecond = sgrade[1];
+		   second = int.Parse(ssecond.ToString());
+		}
+		catch (IndexOutOfRangeException)
+		{
+			second = 0;
+		}
+		
+		
+		if (grade >= 90)
+		{
+			letter = "A";
+		}
+		else if (grade >= 80)
+		{
+			letter = "B";
+		}
+		else if (grade >= 70)
+		{
+			letter = "C";
+		}
+		else if (grade >= 60)
+		{
+			letter = "D";
+		}
+		else
+		{
+			letter = "F";
+		}
+		
+		
+		if (second >= 7 && letter != "A" && letter != "F")
+		{
+			letter = "+"+letter;
+		}
+		else if (second <= 3 && letter != "F")
+		{
+			letter = "-"+letter;
+		}
+		
+		return letter;
+	}
+}
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+	static void Main(string[] args)
+	{
+		Gradebook g = new Gradebook();
+		string grade = g.GetGrade();
+			
+		Console.WriteLine($"Your grade is an {grade}.");	
+	}
 }
