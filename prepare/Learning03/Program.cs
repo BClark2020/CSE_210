95a8c2d4febbd8372c861716c0ac264c445984c3
BClark21 <brenden.t.clark2020@gmail.com>
2025-01-28 19:16:35 -0700

    Hopefully completed the prepare activity 2. I will now start to run and debugg the program if needed.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,58 @@
 using System;
-
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
-    }
+	
+	static void Main(string[] args)
+	{
+		Resume _res = new Resume();
+		bool _var = true;
+		do
+		{
+			Console.Clear();
+			Job _job = new Job();
+			Console.WriteLine("Job Title: ");
+			_job._jobTitle = Console.ReadLine();
+			Console.WriteLine("Company: ");
+			_job._company = Console.ReadLine();
+			
+			bool _var1 = true;
+			do
+			{
+				try
+				{
+					Console.WriteLine("Start Year: ");
+					_job._startYear = int.Parse(Console.ReadLine());
+
+				}
+				catch (FormatException)
+				{
+					
+					Console.WriteLine("ERROR: Please enter an integer.");
+					_var1 = false;
+				}	
+			}while(_var1 == true);
+			
+			_var1 = true;
+			do
+			{
+				try
+				{
+					Console.WriteLine("End Year: ");
+					_job._endYear = int.Parse(Console.ReadLine());
+
+				}
+				catch (FormatException)
+				{
+					
+					Console.WriteLine("ERROR: Please enter an integer.");
+					_var1 = false;
+				}	
+			}while(_var1 == true);
+			
+			_res._jobs.Add(_job);
+		
+		}while(_var == true);
+		
+		_res.Display();
+	}
 }
