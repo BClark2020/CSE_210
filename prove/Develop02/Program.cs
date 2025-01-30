2623c2b2350c19aff1431b4c6e093e86cfad0191
BClark21 <brenden.t.clark2020@gmail.com>
2025-01-30 14:33:24 -0700

    updated

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,11 @@
 using System;
-
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
-    }
+	static void Main(string[] args)
+	{
+		Console.Clear();
+		Journal _journal = new Journal();
+		_journal.menu();
+		
+	}
 }
