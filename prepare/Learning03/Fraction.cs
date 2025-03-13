4338b28eb142dd9446da71ebfea03cfdd792ae3e
BClark21 <brenden.t.clark2020@gmail.com>
2025-03-13 16:21:37 -0600

    I finished the main brunt if coding. I am going to begin testing.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+
+using System;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+class Fraction
+{
+    private int _top = 1;
+    private int _bottom = 1;
+    
+    public int GetTop()
+    {
+        return _top;
+    }
+    
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    
+    public string GetFractionString()
+    {
+        string _fraction = _top + "/" + _bottom;
+        return _fraction; 
+    }
+    
+    public double GetFraction()
+    {
+        double _fraction = (_top/_bottom);
+        return _fraction;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,11 +1,97 @@
 
 using System;
 using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Reflection.Metadata.Ecma335;
+using Microsoft.VisualBasic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac= new Fraction();
+        bool loop = true;
+        bool _var;
+        while(loop)
+        {
+            Console.Clear();
+            Console.Write("1.) Set Top\n2.) Set bottom\n3.) Display Fraction\n4.) Exit Program");   
+            string _option = Console.ReadLine();
+            Thread.Sleep(200);
+            
+            switch(_option)
+            {
+                case "1":
+                _var = false;
+                do{
+                    try
+                    {
+                        Console.Clear();
+                        Console.Write("Top value: ");
+                        int _top = int.Parse(Console.ReadLine());
+                        Thread.Sleep(100);
+                        frac.SetTop(_top); 
+                        _var =  false;
+                    }  catch (FormatException)
+                    {
+                        _var = true;
+                        Console.WriteLine("ERROR: Pleas enter an int value."); 
+                        Thread.Sleep(3000);
+                    }
+                }while(_var);
+                break;
+                
+                case "2":
+                _var = false;
+                do{
+                try
+                {
+                    Console.Clear();
+                    Console.Write("Bottom Value: ");
+                    int _bottom = int.Parse(Console.ReadLine());
+                    Thread.Sleep(100);
+                    if (_bottom == 0)
+                    {
+                        throw new Exception("ERROR: You cannot divide by ZERO");
+                    }
+                    frac.SetBottom(_bottom); 
+                    _var = false;
+                } catch (FormatException)
+                {
+                    Console.WriteLine("ERROR: Pleas enter an int value."); 
+                    Thread.Sleep(3000);
+                    _var =  true;
+                } catch (Exception ex)
+                {
+                    Console.WriteLine($"ERROR: {ex.Message}"); 
+                    Thread.Sleep(3000);
+                    _var = true;
+                }
+                }while(_var);
+                break;
+                
+                case "3": 
+                Console.Clear();
+                string _fractionS = frac.GetFractionString();
+                double _fractionD = frac.GetFraction();
+                Console.WriteLine("Press 'enter' to leave.");
+                Thread.Sleep(100);
+                Console.WriteLine($"Fraction: {_fractionS}");
+                Thread.Sleep(100);
+                Console.WriteLine($"Decimal: {_fractionD}");
+                Console.ReadLine();
+                break; 
+                
+                case "4":
+                loop = false;
+                Console.WriteLine("Thank you for using my program ༼ つ ◕_◕ ༽つ");
+                break;
+                
+                default:
+                Console.WriteLine("Please enter a number between 1 and 4.");
+                Thread.Sleep(3000);
+                break; 
+            }
+        }
+       
     }
 }
